2fc0c431a6d0cb572fe6ccaec03e0f3bdf2457e1
Chris Patuzzo <chris@patuzzo.co.uk>
2025-12-06 19:00:03 +0000

    Add exercise 2.6

---
+++ 02_c_fundamentals/06_underscores.c
@@ -0,0 +1,7 @@
+int main(void) {
+  // It's not a good idea for an identifier to contain more than one adjacent
+  // underscores because it makes the variables hard to read/distinguish.
+  int current__balance;
+  int current___balance;
+  return 0;
+}
