b8f28fd40609fcc1183b5e741c1038ed5156b7e3
Chris Patuzzo <chris@patuzzo.co.uk>
2025-12-06 19:02:20 +0000

    Add exercise 2.7

---
+++ 02_c_fundamentals/07_keywords.c
@@ -0,0 +1,18 @@
+int main(void) {
+  // 'for' is a keyword.
+  // int for;
+
+  // 'If' is not a keyword.
+  int If;
+
+  // 'main' is not a keyword.
+  int main;
+
+  // 'printf' is not a keyword.
+  int printf;
+
+  // 'while' is a keyword.
+  // int while;
+
+  return 0;
+}
