0c626a2bd75c119522f79584e4f11ad4fedbee7c
Chris Patuzzo <chris@patuzzo.co.uk>
2025-12-06 18:54:14 +0000

    Add exercise 2.4

---
+++ 02_c_fundamentals/04_declarations.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+  int a, b, c;
+  float d, e, f;
+
+  printf("a: %d\n", a);
+  printf("b: %d\n", b);
+  printf("c: %d\n", c);
+
+  printf("d: %f\n", (double)d);
+  printf("e: %f\n", (double)e);
+  printf("f: %f\n", (double)f);
+
+  return 0;
+}
