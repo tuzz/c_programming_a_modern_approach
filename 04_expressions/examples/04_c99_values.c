e74738dfd52d1d6c1ad94af0339d5575294dbcc2
Chris Patuzzo <chris@patuzzo.co.uk>
2025-12-07 17:29:22 +0000

    Add exercise 4.4

---
+++ 04_expressions/examples/04_c99_values.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void) {
+  printf("%d\n", 8 / 5); // a) 1
+  printf("%d\n", -8 / 5); // b) -1
+  printf("%d\n", 8 / -5); // c) -1
+  printf("%d\n", -8 / -5); // d) 1
+  return 0;
+}
