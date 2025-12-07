528bdcda647d48d8599902a3cb2578eb32e16ae0
Chris Patuzzo <chris@patuzzo.co.uk>
2025-12-07 19:00:09 +0000

    Add project 4.1

---
+++ 04_expressions/projects/01_digits_reversed.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+  int number;
+  printf("Enter a two-digit number: ");
+  scanf("%d", &number);
+
+  int first_digit = number / 10;
+  int last_digit = number % 10;
+
+  printf("The reversal is: %d%d\n", last_digit, first_digit);
+
+  return 0;
+}
