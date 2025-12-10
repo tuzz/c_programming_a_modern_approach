19f67e624b3647f07f8001295514c38f111810bb
Chris Patuzzo <chris@patuzzo.co.uk>
2025-12-10 12:45:14 +0000

    Add example 5.3

---
+++ 05_selection_statements/examples/03_program_fragments3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void) {
+  int i, j, k;
+
+  // a) Prints "1 3 4 5"
+  i = 3; j = 4; k = 5;
+  printf("%d ", i < j || ++j < k);
+  printf("%d %d %d\n", i, j, k);
+
+  // b) Prints "0 7 8 9"
+  i = 7; j = 8; k = 9;
+  printf("%d ", i - 7 && j++ < k);
+  printf("%d %d %d\n", i, j, k);
+
+  // c) Prints "1 8 8 9"
+  i = 7; j = 8; k = 9;
+  printf("%d ", (i = j) || (j = k));
+  printf("%d %d %d\n", i, j, k);
+
+  // d) Prints "1 2 1 1"
+  i = 1; j = 1; k = 1;
+  printf("%d ", ++i || (++j && ++k));
+  printf("%d %d %d\n", i, j, k);
+
+  return 0;
+}
