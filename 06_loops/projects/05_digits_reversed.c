731218ed0e023f842b9b3a2393105c01a595963d
Chris Patuzzo <chris@patuzzo.co.uk>
2025-12-10 19:52:06 +0000

    Add project 6.5

---
+++ 06_loops/projects/05_digits_reversed.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+  int number;
+  printf("Enter a number: ");
+  scanf("%d", &number);
+
+  printf("The reversal is: ");
+  do {
+    printf("%d", number % 10);
+    number /= 10;
+  } while (number > 0);
+
+  printf("\n");
+  return 0;
+}
