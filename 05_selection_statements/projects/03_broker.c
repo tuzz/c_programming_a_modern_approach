2be035e9ff4ea3a55ef81eef145e99f9484241e5
Chris Patuzzo <chris@patuzzo.co.uk>
2025-12-10 13:59:16 +0000

    Add project 5.3

--- 05_selection_statements/projects/03_broker.c
+++ 05_selection_statements/projects/03_broker.c
@@ -1,29 +1,46 @@
 #include <stdio.h>
 
-int main(void)
-{
-  float commission, value;
+int main(void) {
+  int num_shares;
+  printf("Enter number of shares: ");
+  scanf("%d", &num_shares);
 
-  printf("Enter value of trade: ");
-  scanf("%f", &value);
+  float price_per_share;
+  printf("Enter price per share: ");
+  scanf("%f", &price_per_share);
 
-  if (value < 2500.00f)
-    commission = 30.00f + .017f * value;
-  else if (value < 6250.00f)
-    commission = 56.00f + .0066f * value;
-  else if (value < 20000.00f)
-    commission = 76.00f + .0034f * value;
-  else if (value < 50000.00f)
-    commission = 100.00f + .0022f * value;
-  else if (value < 500000.00f)
-    commission = 155.00f + .0011f * value;
-  else
-    commission = 255.00f + .0009f * value;
+  float value = num_shares * price_per_share;
+  float commission;
 
-  if (commission < 39.00f)
+  if (value < 2500.00f) {
+    commission = 30.00f + 0.017f * value;
+  } else if (value < 6250.00f) {
+    commission = 56.00f + 0.0066f * value;
+  } else if (value < 20000.00f) {
+    commission = 76.00f + 0.0034f * value;
+  } else if (value < 50000.00f) {
+    commission = 100.00f + 0.0022f * value;
+  } else if (value < 500000.00f) {
+    commission = 155.00f + 0.0011f * value;
+  } else {
+    commission = 255.00f + 0.0009f * value;
+  }
+
+  if (commission < 39.00f) {
     commission = 39.00f;
+  }
 
   printf("Commission: $%.2f\n", (double)commission);
+
+  float rival_commission;
+
+  if (num_shares < 2000) {
+    rival_commission = 33.00f + 0.03f * num_shares;
+  } else {
+    rival_commission = 33.00f + 0.02f * num_shares;
+  }
+
+  printf("Rival commission: $%.2f\n", (double)rival_commission);
 
   return 0;
 }
