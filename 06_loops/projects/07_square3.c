2ea3caed0db61e33f3c944662edc4bc64b92089b
Chris Patuzzo <chris@patuzzo.co.uk>
2025-12-10 20:14:50 +0000

    Add project 6.7

--- 06_loops/projects/07_square3.c
+++ 06_loops/projects/07_square3.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
 int main(void) {
-  int i, n, odd, square;
-
   printf("This program prints a table of squares.\n");
+
+  int n;
   printf("Enter number of entries in table: ");
   scanf("%d", &n);
 
-  i = 1;
-  odd = 3;
-  for (square = 1; i <= n; odd += 2) {
+  int square = 1;
+  int odd = 3;
+  for (int i = 1; i <= n; i++) {
     printf("%10d%10d\n", i, square);
-    ++i;
     square += odd;
+    odd += 2;
   }
 
   return 0;
