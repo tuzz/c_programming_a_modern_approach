08891b29529fdf4c63f3cad96cb4cbbff7622d9b
Chris Patuzzo <chris@patuzzo.co.uk>
2025-12-10 19:46:43 +0000

    Add project 6.4

---
+++ 06_loops/projects/04_broker.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(void) {
+  float value;
+
+  while (true) {
+    printf("Enter value of trade: ");
+    scanf("%f", &value);
+    if (value == 0.0f) { break; }
+
+    float commission;
+    if (value < 2500.00f) {
+      commission = 30.00f + 0.017f * value;
+    } else if (value < 6250.00f) {
+      commission = 56.00f + 0.0066f * value;
+    } else if (value < 20000.00f) {
+      commission = 76.00f + 0.0034f * value;
+    } else if (value < 50000.00f) {
+      commission = 100.00f + 0.0022f * value;
+    } else if (value < 500000.00f) {
+      commission = 155.00f + 0.0011f * value;
+    } else {
+      commission = 255.00f + 0.0009f * value;
+    }
+
+    if (commission < 39.00f) { commission = 39.00f; }
+    printf("Commission: $%.2f\n\n", (double)commission);
+  }
+
+  return 0;
+}
