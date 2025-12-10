2f242a3c1c4473886736df30cef5e610d9d9de50
Chris Patuzzo <chris@patuzzo.co.uk>
2025-12-10 19:15:17 +0000

    Add the original primality testing program

---
+++ 06_loops/exercises/12_primality_test.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+  int d, n = 91;
+
+  for (d = 2; d < n; d++)
+    if (n % d == 0)
+      break;
+
+  if (d < n)
+    printf("%d is divisible by %d\n", n, d);
+  else
+    printf("%d is prime\n", n);
+
+  return 0;
+}
