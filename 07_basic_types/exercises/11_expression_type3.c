192f143a99ec294172b7d043e689c49be8d7e79d
Chris Patuzzo <chris@patuzzo.co.uk>
2025-12-13 13:06:39 +0000

    Add exercise 7.11

---
+++ 07_basic_types/exercises/11_expression_type3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+  int i = 1;
+  float f = 1.0f;
+  double d = 1.0;
+
+  // The type of the expression is double. First i * f will be evaluated which
+  // implicitly casts i to a float. Then result / d will be evaluated which
+  // implicitly casts result to a double and the result is double.
+  printf("%f\n", i * f / d);
+
+  return 0;
+}
