7c73b827c19377e82e06eefb78f1bf3d9132d165
Chris Patuzzo <chris@patuzzo.co.uk>
2025-12-14 17:24:23 +0000

    Add the original fact.c program

---
+++ 09_functions/exercises/16_fact.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+static int fact(int n) {
+  if (n <= 1) {
+    return 1;
+  } else {
+    return n * fact(n - 1);
+  }
+}
+
+int main(void) {
+  printf("fact(0) returns %d\n", fact(0));
+  printf("fact(1) returns %d\n", fact(1));
+  printf("fact(2) returns %d\n", fact(2));
+  printf("fact(3) returns %d\n", fact(3));
+  printf("fact(4) returns %d\n", fact(4));
+  return 0;
+}
