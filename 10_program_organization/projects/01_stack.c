0a58f048f819673e904d2376dbe928e72bc4d2db
Chris Patuzzo <chris@patuzzo.co.uk>
2025-12-20 11:14:06 +0000

    Add project 10.1

--- 10_program_organization/projects/01_stack.c
+++ 10_program_organization/projects/01_stack.c
@@ -1,11 +1,17 @@
 #include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #define STACK_SIZE 100
 
-static void stack_overflow(void) {}
+static void stack_overflow(void) {
+  printf("Stack overflow\n");
+  exit(1);
+}
+
 static void stack_underflow(void) {}
 
-static int contents[STACK_SIZE];
+static char contents[STACK_SIZE];
 static int top = 0;
 
 static void make_entry(void) {
@@ -20,23 +26,44 @@ static bool is_full(void) {
   return top == STACK_SIZE;
 }
 
-static void push(int i) {
+static void push(char c) {
   if (is_full()) {
     stack_overflow();
   } else {
-    contents[top++] = i;
+    contents[top++] = c;
   }
 }
 
-static int pop(void) {
+static char pop(void) {
   if (is_empty()) {
     stack_underflow();
-    return 0;
+    return '\0';
   } else {
     return contents[--top];
   }
 }
 
 int main(void) {
+  printf("Enter parentheses and/or braces: ");
+
+  char c, d;
+  while ((c = (char)getchar()) != '\n') {
+    if (c == '(' || c == '{') {
+      push(c);
+    } else if (c == ')') {
+      d = pop();
+      if (d != '(') { push(d); break; }
+    } else if (c == '}') {
+      d = pop();
+      if (d != '{') { push(d); break; }
+    }
+  }
+
+  if (is_empty()) {
+    printf("Parentheses/braces are nested properly\n");
+  } else {
+    printf("Parentheses/braces are NOT nested properly\n");
+  }
+
   return 0;
 }
