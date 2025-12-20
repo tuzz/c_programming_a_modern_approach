aa2d993f5aac856e43c1f938674231a616e1af51
Chris Patuzzo <chris@patuzzo.co.uk>
2025-12-20 20:28:28 +0000

    Add the original maxmin.c program

---
+++ 12_pointers_and_arrays/projects/07_maxmin.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+static void max_min(int a[], int n, int *max, int *min) {
+  int i;
+
+  *max = *min = a[0];
+  for (i = 1; i < n; i++) {
+    if (a[i] > *max)
+      *max = a[i];
+    else if (a[i] < *min)
+      *min = a[i];
+  }
+}
+
+int main(void) {
+  int a[5] = {2, 5, 3, 1, 4};
+
+  int max = 0;
+  int min = 0;
+  max_min(a, 5, &max, &min);
+
+  printf("max: %d\n", max);
+  printf("min: %d\n", min);
+
+  return 0;
+}
