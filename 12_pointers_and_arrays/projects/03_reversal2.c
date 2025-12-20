916200762ae867e33f5fa806f6b72d6b09d2e9ef
Chris Patuzzo <chris@patuzzo.co.uk>
2025-12-20 20:09:43 +0000

    Add project 12.3

---
+++ 12_pointers_and_arrays/projects/03_reversal2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+  printf("Enter a message: ");
+  char message[50], c, *p = message;
+
+  while ((c = (char)getchar()) != '\n' && p < message + 50) {
+    *p++ = c;
+  }
+
+  printf("Reversal is: ");
+  for (p--; p >= message; p--) {
+    printf("%c", *p);
+  }
+  printf("\n");
+
+  return 0;
+}
