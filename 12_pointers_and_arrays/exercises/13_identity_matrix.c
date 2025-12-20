293ee56a613018328346a20ee0aa1ea7d55fc22d
Chris Patuzzo <chris@patuzzo.co.uk>
2025-12-20 19:23:22 +0000

    Add the original identity_matrix.c program

---
+++ 12_pointers_and_arrays/exercises/13_identity_matrix.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+#define N 10
+
+int main(void) {
+  double ident[N][N];
+  int row, col;
+
+  for (row = 0; row < N; row++)
+    for (col = 0; col < N; col++)
+      if (row == col)
+        ident[row][col] = 1.0;
+      else
+        ident[row][col] = 0.0;
+
+
+  for (row = 0; row < N; row++) {
+    for (col = 0; col < N; col++) {
+      printf("%f ", ident[row][col]);
+    }
+    printf("\n");
+  }
+
+  return 0;
+}
