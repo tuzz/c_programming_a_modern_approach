764d7b49de8c231862fb8513f8f9cc8b009b7968
Chris Patuzzo <chris@patuzzo.co.uk>
2025-12-20 18:59:13 +0000

    Add the original find_middle.c program

---
+++ 12_pointers_and_arrays/exercises/10_find_middle.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+#define ARRAY_COUNT(array) (sizeof(array) / sizeof(array[0]))
+
+static int *find_middle(int a[], int n) {
+  return &a[n / 2];
+}
+
+int main(void) {
+  int a[5] = {1, 2, 3, 4, 5};
+
+  int *middle = find_middle(a, ARRAY_COUNT(a));
+  printf("middle: %d\n", *middle);
+
+  return 0;
+}
