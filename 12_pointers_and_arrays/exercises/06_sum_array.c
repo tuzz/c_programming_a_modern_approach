7c2770d7fc5a1d9c3383ed2663620304ad886601
Chris Patuzzo <chris@patuzzo.co.uk>
2025-12-20 18:45:56 +0000

    Add exercise 12.6

--- 12_pointers_and_arrays/exercises/06_sum_array.c
+++ 12_pointers_and_arrays/exercises/06_sum_array.c
@@ -2,12 +2,13 @@
 
 #define ARRAY_COUNT(array) (sizeof(array) / sizeof(array[0]))
 
-static int sum_array(const int a[], int n) {
-  int i, sum;
+static int sum_array(const int *a, int n) {
+  int sum = 0;
 
-  sum = 0;
-  for (i = 0; i < n; i++)
-    sum += a[i];
+  for (const int *p = a; p < &a[n]; p++) {
+    sum += *p;
+  }
+
   return sum;
 }
 
