1b02911bb3dcc3ab47b0efcbaf01145a0ee7ce54
Chris Patuzzo <chris@patuzzo.co.uk>
2025-12-21 13:55:46 +0000

    Add exercise 13.13

---
+++ 13_strings/exercises/13_build_index_url.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+static void build_index_url(const char *domain, char *index_url) {
+  strcpy(index_url, "http://www.");
+  strcat(index_url, domain);
+  strcat(index_url, "/index.html");
+}
+
+int main(void) {
+  char index_url[100] = "";
+
+  build_index_url("knking.com", index_url);
+  printf("%s\n", index_url);
+
+  return 0;
+}
