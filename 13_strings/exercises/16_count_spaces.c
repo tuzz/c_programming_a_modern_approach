36e3a2ef6e0bbc424fe8b870a9fb0c0f74c89dcd
Chris Patuzzo <chris@patuzzo.co.uk>
2025-12-21 14:57:23 +0000

    Add exercise 13.16

--- 13_strings/exercises/16_count_spaces.c
+++ 13_strings/exercises/16_count_spaces.c
@@ -3,9 +3,12 @@
 static int count_spaces(const char *s) {
   int count = 0;
 
-  for (; *s != '\0'; s++)
-    if (*s == ' ')
+  while (*s) {
+    if (*s++ == ' ') {
       count++;
+    }
+  }
+
   return count;
 }
 
