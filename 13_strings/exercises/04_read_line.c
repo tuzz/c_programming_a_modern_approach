98488b69d28522cc6b698898529661978502f824
Chris Patuzzo <chris@patuzzo.co.uk>
2025-12-21 12:49:57 +0000

    Add exercise 13.4

--- 13_strings/exercises/04_read_line.c
+++ 13_strings/exercises/04_read_line.c
@@ -1,21 +1,32 @@
 #include <stdio.h>
+#include <ctype.h>
 
 static int read_line(char str[], int n) {
   int ch, i = 0;
 
-  while ((ch = getchar()) != '\n' && ch != EOF) {
-    if (i < n) {
+  // Skip leading whitespace.
+  while (isspace((ch = getchar())) && ch != EOF) {}
+
+  do {
+    // Leave behind characters that we don't have room to store (reserve space for \0).
+    if (i < n - 1) {
       str[i++] = (char)ch;
+
+      // Stop reading at the first new-line character (stored in the string).
+      if (ch == '\n') { break; }
+    } else {
+      break;
     }
-  }
+  } while ((ch = getchar()) != EOF);
+
   str[i] = '\0';
   return i;
 }
 
 int main(void) {
-  char str[50];
-  read_line(str, 50);
+  char str[10];
+  read_line(str, 10);
 
-  printf("str: %s\n", str);
+  printf("str: %s", str);
   return 0;
 }
