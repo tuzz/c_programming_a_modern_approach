d75bf38df944553a0a78abcd1a5cfba3a938a630
Chris Patuzzo <chris@patuzzo.co.uk>
2025-12-21 17:36:12 +0000

    Add project 13.5

---
+++ 13_strings/projects/05_sum.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+  int total = 0;
+
+  for (int i = 1; i < argc; i++) {
+    total += atoi(argv[i]);
+  }
+
+  printf("Total: %d\n", total);
+  return 0;
+}
