76c0f280bb4611a891919d222582b067dd3163d7
Chris Patuzzo <chris@patuzzo.co.uk>
2025-12-22 09:31:24 +0000

    Add project 13.8

---
+++ 13_strings/projects/08_scrabble.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <ctype.h>
+
+static int compute_scrabble_value(const char *word) {
+  int sum = 0;
+
+  while (*word) {
+    char c = (char)toupper(*word);
+
+    switch (c) {
+      case 'A': case 'E': case 'I': case 'L': case 'N': case 'O': case 'R': case 'S': case 'T': case 'U':
+        sum += 1;
+        break;
+      case 'D': case 'G':
+        sum += 2;
+        break;
+      case 'B': case 'C': case 'M': case 'P':
+        sum += 3;
+        break;
+      case 'F': case 'H': case 'V': case 'W': case 'Y':
+        sum += 4;
+        break;
+      case 'K':
+        sum += 5;
+        break;
+      case 'J': case 'X':
+        sum += 8;
+        break;
+      case 'Q': case 'Z':
+        sum += 10;
+        break;
+      default:
+        break;
+    }
+
+    word++;
+  }
+
+  return sum;
+}
+
+int main(void) {
+  printf("Enter a word: ");
+  char c, word[50] = "", *p = word;
+
+  while ((c = (char)getchar()) != '\n') { *p++ = c; }
+
+  printf("Scrabble value: %d\n", compute_scrabble_value(word));
+  return 0;
+}
