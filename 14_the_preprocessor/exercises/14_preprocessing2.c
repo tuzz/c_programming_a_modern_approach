390d9515e6687e32f58cd8f0b12f6ded632d8d9d
Chris Patuzzo <chris@patuzzo.co.uk>
2025-12-22 14:57:09 +0000

    Add exercise 14.14

--- 14_the_preprocessor/exercises/14_preprocessing2.c
+++ 14_the_preprocessor/exercises/14_preprocessing2.c
@@ -1,32 +1,33 @@
-#define N = 10
-#define INC(x) x+1
-#define SUB (x,y) x-y
-#define SQR(x) ((x)*(x))
-#define CUBE(x) (SQR(x)*(x))
-#define M1(x,y) x##y
-#define M2(x,y) #x #y
+
+
+
+
+
+
+
 
 int main(void)
 {
-  int a[N], i, j, k, m;
+  int a[= 10], i, j, k, m;
 
-#ifdef N
+
   i = j;
-#else
-  j = i;
-#endif
 
-  i = 10 * INC(j);
-  i = SUB(j, k);
-  i = SQR(SQR(j));
-  i = CUBE(j);
-  i = M1(j, k);
+
+
+
+  i = 10 * j+1
+  i = (x,y) x-y(j, k);
+  i = ((((j)*(j))*((j)*(j))));
+  i = (((j)*(j))*(j));
+  i = jk;
   puts(M2(i, j));
+  puts("i" "j");
 
-#undef SQR
+
   i = SQR(j);
-#define SQR
-  i = SQR(j);
+
+  i = (j);
 
   return 0;
 }
