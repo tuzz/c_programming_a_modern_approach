f9267bab2fc09a2c0e12d99b1e325e9bd450e7cc
Chris Patuzzo <chris@patuzzo.co.uk>
2025-12-22 14:46:21 +0000

    Add exercise 14.13

--- 14_the_preprocessor/exercises/13_preprocessing.c
+++ 14_the_preprocessor/exercises/13_preprocessing.c
@@ -1,23 +1,17 @@
-#include <stdio.h>
-
-#define N 100
+// Included lines from stdio.h
 
 void f(void);
 
 int main(void)
 {
   f();
-#ifdef N
-#undef N
-#endif
   return 0;
 }
 
 void f(void)
 {
-#if defined(N)
-  printf("N is %d\n", N);
-#else
   printf("N is undefined\n");
-#endif
 }
+
+
+// The output of this program will therefore be "N is undefined\n"
