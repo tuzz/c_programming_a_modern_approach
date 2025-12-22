6818b7193f18e88e6f9c9000263a4f69b396e6ea
Chris Patuzzo <chris@patuzzo.co.uk>
2025-12-22 08:50:54 +0000

    Add project 13.6

--- 13_strings/projects/06_planet.c
+++ 13_strings/projects/06_planet.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
+#include <ctype.h>
 
 #define NUM_PLANETS 9
+
+static bool uppercase_equal(const char *s1, const char *s2) {
+  for (; *s1 && *s2; s1++, s2++) {
+    if (toupper(*s1) != toupper(*s2)) {
+      return false;
+    }
+  }
+
+  return *s1 == *s2;
+}
 
 int main(int argc, char **argv) {
   char *planets[] = {"Mercury", "Venus", "Earth",
@@ -11,7 +23,7 @@ int main(int argc, char **argv) {
 
   for (i = 1; i < argc; i++) {
     for (j = 0; j < NUM_PLANETS; j++)
-      if (strcmp(argv[i], planets[j]) == 0) {
+      if (uppercase_equal(argv[i], planets[j])) {
         printf("%s is planet %d\n", argv[i], j + 1);
         break;
       }
