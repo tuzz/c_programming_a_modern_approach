994c708cc8a4a13d33e660360db37f4b9b0fbd4f
Chris Patuzzo <chris@patuzzo.co.uk>
2025-12-23 12:43:27 +0000

    Add exercise 16.15

---
+++ 16_structures_unions_and_enumerations/exercises/15_weekdays.c
@@ -0,0 +1,9 @@
+// a)
+// enum weekday { MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY }
+
+// b)
+typedef enum { MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY } weekday;
+
+int main(void) {
+  return 0;
+}
