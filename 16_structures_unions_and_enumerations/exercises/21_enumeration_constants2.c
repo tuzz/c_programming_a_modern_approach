8350ed7a8ab07087fa380b0bc101dce7146cbd32
Chris Patuzzo <chris@patuzzo.co.uk>
2025-12-23 13:27:57 +0000

    Add exercise 16.21

---
+++ 16_structures_unions_and_enumerations/exercises/21_enumeration_constants2.c
@@ -0,0 +1,8 @@
+enum a {NUL, SOH, STX, ETX}; // 0, 1, 2, 3
+enum b {VT = 11, FF, CR}; // 11, 12, 13
+enum c {SO = 14, SI, DLE, CAN = 24, EM}; // 14, 15, 16, 24, 25
+enum d {ENQ = 45, ACK, BEL, LF = 37, ETB, ESC}; // 45, 46, 47, 37, 38, 39
+
+int main(void) {
+  return 0;
+}
