d5d49dfc0c13cd338712737bbc69c03af5ca2c9a
Chris Patuzzo <chris@patuzzo.co.uk>
2025-12-23 10:59:10 +0000

    Add exercise 16.2

---
+++ 16_structures_unions_and_enumerations/exercises/02_declare_struct1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void) {
+  // a)
+  // struct { double real, imaginary; } c1, c2, c3;
+
+  // b)
+  struct { double real, imaginary; }
+    c1 = { .real = 0.0, .imaginary = 1.0 },
+    c2 = { .real = 1.0, .imaginary = 0.0 },
+    c3;
+
+  // c) Yes, it can be done in one statement.
+  c1 = c2;
+
+  // d)
+  c3.real = c1.real + c2.real;
+  c3.imaginary = c1.imaginary + c2.imaginary;
+
+  printf("c3: real=%f, imaginary=%f\n", c3.real, c3.imaginary);
+  return 0;
+}
