48855a297fa5f0dab4f3944a9ab448b24da40cc9
Chris Patuzzo <chris@patuzzo.co.uk>
2025-12-28 18:00:37 +0000

    Add project 17.4

--- 17_advanced_uses_of_pointers/projects/04_justify.c
+++ 17_advanced_uses_of_pointers/projects/04_justify.c
@@ -1,27 +1,52 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 #define MAX_LINE_LEN 60
 #define MAX_WORD_LEN 20
 
-static char line[MAX_LINE_LEN+1];
+struct node {
+  struct node *next;
+  char word[];
+};
+
+static struct node *line = NULL;
 static int line_len = 0;
 static int num_words = 0;
 
 static void clear_line(void) {
-  line[0] = '\0';
+  for (struct node *p = line, *next; p; p = next) {
+    next = p->next;
+    free(p);
+  }
+  line = NULL;
   line_len = 0;
   num_words = 0;
 }
 
 static void add_word(const char *word) {
+  unsigned long word_len = strlen(word);
+  struct node *new_node = calloc(1, sizeof(*new_node) + word_len + 1);
+
+  if (!new_node) {
+    printf("Failed to allocate memory.\n");
+    exit(EXIT_FAILURE);
+  }
+
+  strcpy(new_node->word, word);
+
+  if (line) {
+    struct node *p;
+    for (p = line; p->next; p = p->next) {}
+    p->next = new_node;
+  } else {
+    line = new_node;
+  }
+
+  line_len += word_len;
   if (num_words > 0) {
-    line[line_len] = ' ';
-    line[line_len+1] = '\0';
     line_len++;
   }
-  strcat(line, word);
-  line_len += strlen(word);
   num_words++;
 }
 
@@ -31,25 +56,38 @@ static int space_remaining(void) {
 
 static void write_line(void) {
   int extra_spaces, spaces_to_insert, i, j;
+  extra_spaces = space_remaining();
 
-  extra_spaces = MAX_LINE_LEN - line_len;
-  for (i = 0; i < line_len; i++) {
-    if (line[i] != ' ')
-      putchar(line[i]);
-    else {
+  struct node *p = line;
+  int count = 0;
+
+  while (p && count < line_len) {
+    printf("%s", p->word);
+
+    if (num_words > 1) {
       spaces_to_insert = extra_spaces / (num_words - 1);
       for (j = 1; j <= spaces_to_insert + 1; j++)
         putchar(' ');
       extra_spaces -= spaces_to_insert;
-      num_words--;
     }
+    count += strlen(p->word);
+    num_words--;
+    p = p->next;
   }
   putchar('\n');
 }
 
 static void flush_line(void) {
-  if (line_len > 0)
-    puts(line);
+  if (line_len > 0) {
+    for (struct node *p = line; p; p = p->next) {
+      if (p != line) {
+        putchar(' ');
+      }
+      printf("%s ", p->word);
+    }
+  }
+  printf("\n");
+  clear_line();
 }
 
 static int read_char(void) {
