b1a07ab4cf9475b4c474baacc4088c79f1864ba5
Chris Patuzzo <chris@patuzzo.co.uk>
2025-12-28 13:48:41 +0000

    Add exercise 17.6

--- 17_advanced_uses_of_pointers/exercises/06_delete_from_list.c
+++ 17_advanced_uses_of_pointers/exercises/06_delete_from_list.c
@@ -8,26 +8,43 @@ struct node {
 };
 
 static struct node *delete_from_list(struct node *list, int n) {
-  struct node *cur, *prev;
+  // Check if the list is empty.
+  if (!list) { return list; }
 
-  for (cur = list, prev = NULL;
-      cur != NULL && cur->value != n;
-      prev = cur, cur = cur->next)
-    ;
+  struct node *p = list;
 
-  if (cur == NULL)
-    return list;
-  if (prev == NULL)
+  // Check if n is at the start of the list.
+  if (list->value == n) {
     list = list->next;
-  else
-    prev->next = cur->next;
-  free(cur);
+    free(p);
+    return list;
+  }
+
+  // Find the node immediately before n.
+  while (p->next && p->next->value != n) {
+    p = p->next;
+  }
+
+  // Shift the value of node i+2 into i+1.
+  while (p->next && p->next->next) {
+    p->next->value = p->next->next->value;
+    p = p->next;
+  }
+
+  // Discard the surplus node at the end.
+  free(p->next);
+  p->next = NULL;
+
+  // Is this really what the exercise wants? If so, it's asking us to implement
+  // a less efficient algorithm O(N) to save one variable and requires us to
+  // come up with a novel ideal to treat the linked list like a normal array.
+
   return list;
 }
 
 int main(void) {
   struct node *a = calloc(1, sizeof(*a));
-  struct node *b = calloc(1, sizeof(*a));
+  struct node *b = calloc(1, sizeof(*b));
 
   a->value = 123;
   b->value = 456;
