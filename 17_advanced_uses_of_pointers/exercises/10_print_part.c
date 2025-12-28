86470c7fd077050b1dbfb66413c8e1d5436a187a
Chris Patuzzo <chris@patuzzo.co.uk>
2025-12-28 14:32:29 +0000

    Add exercise 17.10

--- 17_advanced_uses_of_pointers/exercises/10_print_part.c
+++ 17_advanced_uses_of_pointers/exercises/10_print_part.c
@@ -9,14 +9,14 @@ struct part {
   char _padding[3];
 };
 
-static void print_part(struct part p) {
-  printf("Part number: %d\n", p.number);
-  printf("Part name: %s\n", p.name);
-  printf("Quantity on hand: %d\n", p.on_hand);
+static void print_part(struct part *p) {
+  printf("Part number: %d\n", p->number);
+  printf("Part name: %s\n", p->name);
+  printf("Quantity on hand: %d\n", p->on_hand);
 }
 
 int main(void) {
   struct part p = { .number = 123, .name = "something", .on_hand = 5 };
-  print_part(p);
+  print_part(&p);
   return 0;
 }
