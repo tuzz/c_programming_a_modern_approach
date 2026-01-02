05d47e00bfd387eef94c153c3aea57ceb08a646b
Chris Patuzzo <chris@patuzzo.co.uk>
2026-01-02 16:37:35 +0000

    Add exercise 19.1

---
+++ 19_program_design/exercises/01_queue.h
@@ -0,0 +1,14 @@
+// Inserts an item at the end of the queue. Returns false if the queue is full.
+bool queue_push_back(Item *item);
+
+// Removes an item from the beginning of the queue. Returns NULL if the queue is empty.
+Item *queue_pop_front(void);
+
+// Returns the first item in the queue without changing the queue. Returns NULL if the queue is empty.
+Item *queue_first(void);
+
+// Returns the last item in the queue without changing the queue. Returns NULL if the queue is empty.
+Item *queue_last(void);
+
+// Tests whether the queue is empty.
+bool queue_is_empty(void);
