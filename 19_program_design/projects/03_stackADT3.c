3638e6ffda875cfa3394e4ea2413504b858c9101
Chris Patuzzo <chris@patuzzo.co.uk>
2026-01-03 13:52:18 +0000

    Add project 19.3

---
+++ 19_program_design/projects/03_stackADT3.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "03_stackADT3.h"
+
+struct node {
+  Item data;
+  char _padding[4];
+  struct node *next;
+};
+
+struct stack_type {
+  struct node *top;
+  int len;
+  char _padding[4];
+};
+
+static void terminate(const char *message) {
+  printf("%s\n", message);
+  exit(EXIT_FAILURE);
+}
+
+Stack create(void) {
+  Stack s = malloc(sizeof(struct stack_type));
+  if (s == NULL)
+    terminate("Error in create: stack could not be created.");
+  s->top = NULL;
+  s->len = 0;
+  return s;
+}
+
+void destroy(Stack s) {
+  make_empty(s);
+  free(s);
+}
+
+void make_empty(Stack s) {
+  while (!is_empty(s))
+    pop(s);
+}
+
+bool is_empty(Stack s) {
+  return s->top == NULL;
+}
+
+bool is_full(Stack s) {
+  return false;
+}
+
+void push(Stack s, Item i) {
+  struct node *new_node = malloc(sizeof(struct node));
+  if (new_node == NULL)
+    terminate("Error in push: stack is full.");
+
+  new_node->data = i;
+  new_node->next = s->top;
+  s->top = new_node;
+  s->len++;
+}
+
+Item pop(Stack s) {
+  struct node *old_top;
+  Item i;
+
+  if (is_empty(s))
+    terminate("Error in pop: stack is empty.");
+
+  old_top = s->top;
+  i = old_top->data;
+  s->top = old_top->next;
+  s->len--;
+  free(old_top);
+  return i;
+}
+
+Item peek(Stack s) {
+  if (is_empty(s))
+    terminate("Error in peek: stack is empty.");
+  return s->top->data;
+}
+
+int length(Stack s) {
+  return s->len;
+}
+
+int main(void) {
+  Stack stack = create();
+
+  printf("length: %d\n", length(stack));
+  push(stack, 123);
+  printf("length: %d\n", length(stack));
+  printf("peeked: %d\n", peek(stack));
+  printf("length: %d\n", length(stack));
+  printf("peeked: %d\n", peek(stack));
+  printf("length: %d\n", length(stack));
+  printf("%d\n", pop(stack));
+  printf("length: %d\n", length(stack));
+  printf("%d\n", pop(stack));
+  printf("length: %d\n", length(stack));
+
+  return 0;
+}
