a6f35307b32e17eaf7079bec0c29adfd05cf99ad
Chris Patuzzo <chris@patuzzo.co.uk>
2026-01-03 14:51:21 +0000

    Add project 19.6

--- 19_program_design/projects/06_queueADT.c
+++ 19_program_design/projects/06_queueADT.c
@@ -1,15 +1,14 @@
-#include "05_queueADT.h"
+#include "06_queueADT.h"
 
-#define QUEUE_SIZE 3
 #define PUBLIC
 #define PRIVATE static
 
 struct queue_type {
-  Item *queued_items[QUEUE_SIZE];
-  char _padding[4];
+  Item *queued_items;
   int first_empty_slot;
   int next_to_remove;
   int queue_count;
+  int size;
 };
 
 PRIVATE void terminate(const char *message) {
@@ -17,52 +16,59 @@ PRIVATE void terminate(const char *message) {
   exit(EXIT_FAILURE);
 }
 
-PUBLIC Queue queue_create(void) {
-  Queue queue = calloc(1, sizeof(Queue));
+PUBLIC Queue queue_create(int size) {
+  Queue queue = calloc(1, sizeof(struct queue_type));
   if (!queue) { terminate("Failed to create queue."); }
+
+  queue->queued_items = calloc((unsigned long)size, sizeof(Item));
+  if (!queue->queued_items) { terminate("Failed to create queued_items."); }
+
+  queue->size = size;
   return queue;
 }
 
 PUBLIC void queue_destroy(Queue q) {
+  free(q->queued_items);
   free(q);
 }
 
-PUBLIC bool queue_push_back(Queue q, Item *item) {
-  if (q->queue_count == QUEUE_SIZE) {
+PUBLIC bool queue_push_back(Queue q, Item item) {
+  if (q->queue_count == q->size) {
     return false;
   } else {
     q->queued_items[q->first_empty_slot++] = item;
-    q->first_empty_slot %= QUEUE_SIZE;
+    q->first_empty_slot %= q->size;
     q->queue_count++;
     return true;
   }
 }
 
-PUBLIC Item *queue_pop_front(Queue q) {
+PUBLIC Item queue_pop_front(Queue q) {
   if (queue_is_empty(q)) {
-    return NULL;
+    terminate("Cannot pop from an empty queue.");
+    return 0;
   } else {
-    Item *removed = q->queued_items[q->next_to_remove];
-    q->queued_items[q->next_to_remove++] = NULL;
-    q->next_to_remove %= QUEUE_SIZE;
+    Item removed = q->queued_items[q->next_to_remove];
+    q->queued_items[q->next_to_remove++] = 0;
+    q->next_to_remove %= q->size;
     q->queue_count--;
     return removed;
   }
 }
 
-PUBLIC Item *queue_first(Queue q) {
+PUBLIC Item queue_first(Queue q) {
   if (queue_is_empty(q)) {
-    return NULL;
+    return 0;
   } else {
     return q->queued_items[q->next_to_remove];
   }
 }
 
-PUBLIC Item *queue_last(Queue q) {
+PUBLIC Item queue_last(Queue q) {
   if (queue_is_empty(q)) {
-    return NULL;
+    return 0;
   } else if (q->first_empty_slot == 0) {
-    return q->queued_items[QUEUE_SIZE - 1];
+    return q->queued_items[q->size - 1];
   } else {
     return q->queued_items[q->first_empty_slot - 1];
   }
---
+++ 19_program_design/projects/06_queueclient.c
@@ -0,0 +1,41 @@
+typedef char Item;
+#include "06_queueADT.c"
+
+int main(void) {
+  Queue q1 = queue_create(3);
+  Queue q2 = queue_create(3);
+  bool pushed;
+
+  pushed = queue_push_back(q1, 'a');
+  printf("q1 %s: %c\n", pushed ? "pushed" : "failed to push", 'a');
+
+  pushed = queue_push_back(q1, 'a');
+  printf("q1 %s: %c\n", pushed ? "pushed" : "failed to push", 'b');
+
+  printf("\n");
+
+  pushed = queue_push_back(q2, 'b');
+  printf("q2 %s: %c\n", pushed ? "pushed" : "failed to push", 'b');
+
+  pushed = queue_push_back(q2, 'a');
+  printf("q2 %s: %c\n", pushed ? "pushed" : "failed to push", 'a');
+
+  printf("\n");
+
+  while (!queue_is_empty(q1)) {
+    Item item = queue_pop_front(q1);
+    printf("q1 popped: %c\n", item);
+  }
+
+  printf("\n");
+
+  while (!queue_is_empty(q2)) {
+    Item item = queue_pop_front(q2);
+    printf("q2 popped: %c\n", item);
+  }
+
+  queue_destroy(q1);
+  queue_destroy(q2);
+
+  return 0;
+}
