6299709a4d13f181f2baab4babcfe11dc3ad5d2e
Chris Patuzzo <chris@patuzzo.co.uk>
2026-01-11 13:11:33 +0000

    Add exercise 22.12

--- 22_input_output/exercises/12_count_periods.c
+++ 22_input_output/exercises/12_count_periods.c
@@ -3,10 +3,15 @@
 int count_periods(const char *filename) {
   FILE *fp;
   int n = 0;
+  int ch;
 
   if ((fp = fopen(filename, "r")) != NULL) {
-    while (fgetc(fp) != EOF)
-      if (fgetc(fp) == '.')
+    // The program would incorrectly call fgetc twice which meant that the
+    // result of the first call was ignored, effectively skipping every second
+    // character in the file. The function would return the wrong result. We can
+    // fix it by assigning the result of fgetc to a variable then reuse it.
+    while ((ch = fgetc(fp)) != EOF)
+      if (ch == '.')
         n++;
     fclose(fp);
   }
