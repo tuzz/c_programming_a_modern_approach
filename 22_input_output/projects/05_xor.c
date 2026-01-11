1839480558880056c00d7e6d9f1db3c2144dc81e
Chris Patuzzo <chris@patuzzo.co.uk>
2026-01-11 17:03:10 +0000

    Add project 22.5

--- 22_input_output/projects/05_xor.c
+++ 22_input_output/projects/05_xor.c
@@ -1,20 +1,45 @@
-#include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #define KEY '&'
 
-int main(void) {
-  int orig_char, new_char;
+int main(int argc, char **argv) {
+  if (argc != 3) {
+    fprintf(stderr, "Usage: xor <input> <output>\n");
+    exit(EXIT_FAILURE);
+  }
 
-  while ((orig_char = getchar()) != EOF) {
-    new_char = orig_char ^ KEY;
+  FILE *input = fopen(argv[1], "rb");
+  if (!input) {
+    fprintf(stderr, "Failed to open %s for reading.\n", argv[1]);
+    exit(EXIT_FAILURE);
+  }
 
-    if (isprint(orig_char) && isprint(new_char)) {
-      putchar(new_char);
-    } else {
-      putchar(orig_char);
+  FILE *output = fopen(argv[2], "wb");
+  if (!output) {
+    fprintf(stderr, "Failed to open %s for writing.\n", argv[2]);
+    exit(EXIT_FAILURE);
+  }
+
+  int ch = 0;
+
+  while ((ch = getc(input)) != EOF) {
+    if (putc(ch ^ KEY, output) == EOF) {
+      fprintf(stderr, "Failed to write %s.\n", argv[2]);
+      fclose(input);
+      fclose(output);
+      exit(EXIT_FAILURE);
     }
   }
 
+  if (ferror(input)) {
+    fprintf(stderr, "Failed to read %s.\n", argv[1]);
+    fclose(input);
+    fclose(output);
+    exit(EXIT_FAILURE);
+  }
+
+  fclose(input);
+  fclose(output);
   return 0;
 }
