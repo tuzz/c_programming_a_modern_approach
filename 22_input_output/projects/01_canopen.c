df3190edaa1cc6bf6a794e4f58b5afa5ea541868
Chris Patuzzo <chris@patuzzo.co.uk>
2026-01-11 16:11:56 +0000

    Add project 22.1

--- 22_input_output/projects/01_canopen.c
+++ 22_input_output/projects/01_canopen.c
@@ -1,20 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 int main(int argc, char *argv[]) {
   FILE *fp;
 
-  if (argc != 2) {
-    printf("usage: canopen filename\n");
+  if (argc < 2) {
+    printf("usage: canopen filenames\n");
     exit(EXIT_FAILURE);
   }
 
-  if ((fp = fopen(argv[1], "r")) == NULL) {
-    printf("%s can't be opened\n", argv[1]);
-    exit(EXIT_FAILURE);
+  bool success = true;
+
+  for (int i = 1; i < argc; i++) {
+    if ((fp = fopen(argv[i], "r")) == NULL) {
+      printf("%s can't be opened\n", argv[i]);
+      success = false;
+    } else {
+      printf("%s can be opened\n", argv[i]);
+    }
   }
 
-  printf("%s can be opened\n", argv[1]);
-  fclose(fp);
-  return 0;
+  if (success) {
+    return 0;
+  } else {
+    exit(EXIT_FAILURE);
+  }
 }
